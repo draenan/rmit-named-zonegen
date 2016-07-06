1309be4c77eac59ae82a92bebb295ba47931602f
Adrian Waters <adrian.waters@rmit.edu.au>
2016-07-06 13:39:44 +1000

    Refine building of zone stanza output.

--- named-zonegen.sh
+++ named-zonegen.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
+MASTERS="10.84.196.1; 10.68.196.1;"
+FILEPATH="/path"
+
 for line in $(cat rmit_au_internal.txt); do
     echo $line | grep -q /
     if [ "$?" -eq 1 ]; then
-        zone=$line
+        zone="$line"
+        file="$line"
     else
-        mask=${line#*/}
-        netaddr=${line%/*}
-        octets=$(($mask/8))
+        mask="${line#*/}"
+        netaddr="${line%/*}"
+        octets="$(($mask/8))"
         [[ $netaddr =~ ([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.[0-9]{1,3} ]]
-        case $octets in
-            1)
-                zone=${BASH_REMATCH[1]}.in-addr.arpa
-                ;;
-            2)
-                zone=${BASH_REMATCH[2]}.${BASH_REMATCH[1]}.in-addr.arpa
-                ;;
-            3)
-                zone=${BASH_REMATCH[3]}.${BASH_REMATCH[2]}.${BASH_REMATCH[1]}.in-addr.arpa
-                ;;
-            *)
-                echo WTF?
-                exit 1
-                ;;
-        esac
+        zone="in-addr.arpa"
+        file="rev"
+        for (( i=1; i<=$octets; i++ )); do
+            zone="${BASH_REMATCH[$i]}.$zone"
+            file="${BASH_REMATCH[$(($octets + 1 - $i))]}.$file"
+        done
     fi
 
     cat << EOL
-zone "$zone" { type slave; masters { 10.84.196.1; }; file "/path/${line%/*}" };
+zone "$zone" { type slave; masters { $MASTERS }; file "${FILEPATH}/$file" };
 EOL
 
 done
