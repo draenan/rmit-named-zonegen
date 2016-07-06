de90438d78d7e7818b46ff84daed9e306edbc13a
Adrian Waters <adrian.waters@rmit.edu.au>
2016-07-06 14:36:17 +1000

    Clean things up, be more informative.

--- named-zonegen.sh
+++ named-zonegen.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+#
+# named-zonegen.sh
+#
+# Queries Infoblox for the list of authoritative zones and then uses it to
+# build BIND-style zone statements.
+#
+# Outputs to STDOUT, redirect as necessary.
+#
+# NOTE: Next to no error checking, supplied AS-IS, NO WARRANTY, no liability
+#       admitted to, etc.
+#
 
+GRIDMASTER="infoblox.mgmt.its.rmit.edu.au"
+WAPI_VER="1.7.3"
+DNS_VIEW="AU%20Internal"
+MAX_RESULTS="2500"
 MASTERS="10.84.196.1; 10.68.196.1;"
-FILEPATH="/path"
+ZONEFILE_PATH="/path"
 
-for line in $(cat rmit_au_internal.txt); do
-    echo $line | grep -q /
+API_QUERY_URL="https://${GRIDMASTER}/wapi/v${WAPI_VER}/zone_auth?view=${DNS_VIEW}&_max_results=${MAX_RESULTS}&_return_fields=fqdn"
+TEMPFILE=$(mktemp)
+
+>&2 echo Querying $GRIDMASTER for list of authoritative DNS zones ...
+curl -u $USER -s -k $API_QUERY_URL | awk -F\" '/fqdn/ {print $4}' | sort -n > $TEMPFILE
+if [ ! -s "$TEMPFILE" ]; then
+    >&2 echo Error occurred.
+    rm $TEMPFILE
+    exit 1
+fi
+
+>&2 echo Generating zone config ...
+for line in $(cat $TEMPFILE); do
+    [[ $line =~ / ]]
     if [ "$?" -eq 1 ]; then
         zone="$line"
         file="$line"
@@ -17,12 +44,15 @@ for line in $(cat rmit_au_internal.txt); do
         file="rev"
         for (( i=1; i<=$octets; i++ )); do
             zone="${BASH_REMATCH[$i]}.$zone"
-            file="${BASH_REMATCH[$(($octets + 1 - $i))]}.$file"
+            file="${BASH_REMATCH[ $(( ${octets} + 1 - ${i} )) ]}.$file"
         done
     fi
 
     cat << EOL
-zone "$zone" { type slave; masters { $MASTERS }; file "${FILEPATH}/$file" };
+zone "$zone" { type slave; masters { $MASTERS }; file "${ZONEFILE_PATH}/$file" };
 EOL
 
 done
+
+rm $TEMPFILE
+exit
